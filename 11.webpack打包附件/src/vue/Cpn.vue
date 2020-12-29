ae4fee6e5a5f5df973bdf1635f2ff0669bc3df84
ZZK <1042134104@qq.com>
2020-12-29 22:03:11 +0800

    12.29

---
+++ 11.webpack打包附件/src/vue/Cpn.vue
@@ -0,0 +1,21 @@
+<template>
+      <div>
+    <h2>CPN的标题</h2>
+    <p>cpn的组件</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Cpn',
+  data(){
+   return {
+     name:'cpn组件的name'
+   }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
